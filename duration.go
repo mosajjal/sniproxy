ab6e0d26fff9499b9acd35189697451f9ddd8c64
Alireza Mosajjal <mosajjal@gmail.com>
2022-04-15 12:21:38 +1200

    bugfixes and coversion to POSIX flags

---
+++ duration.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+)
+
+type Duration struct {
+	time.Duration
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		d.Duration = time.Duration(value)
+		return nil
+	case string:
+		var err error
+		d.Duration, err = time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		return errors.New("invalid duration")
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/miekg/dns v1.1.48
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/pflag v1.0.5
 )
 
 require (

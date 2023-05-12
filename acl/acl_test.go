16f2953484bbfd390e4f8884ec6364c06b8e6051
Ali Mosajjal <hi@n0p.me>
2023-05-13 11:19:19 +1200

    added back previous tests and fixed the run function

--- acl/acl_test.go
+++ acl/acl_test.go
@@ -145,10 +145,34 @@ func TestMakeDecision(t *testing.T) {
 
 	// Run the test cases
 	for _, tc := range cases {
-		MakeDecision(tc.connInfo, getAcls(&logger, tc.config))
-		if tc.expected != tc.connInfo.Decision {
-			t.Errorf("MakeDecision (domain=%v,ip=%v,config=%v) decided %v, expected %v", tc.connInfo.Domain, tc.connInfo.SrcIP, tc.config, tc.connInfo.Decision, tc.expected)
-		}
+		t.Run(tc.config, func(t *testing.T) {
+			MakeDecision(tc.connInfo, getAcls(&logger, tc.config))
+			if tc.expected != tc.connInfo.Decision {
+				t.Errorf("MakeDecision (domain=%v,ip=%v,config=%v) decided %v, expected %v", tc.connInfo.Domain, tc.connInfo.SrcIP, tc.config, tc.connInfo.Decision, tc.expected)
+			}
+		})
+	}
+}
+
+// TestReverse tests the reverse function
+func TestReverse(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want string
+	}{
+		{name: "test1", s: "abc", want: "cba"},
+		{name: "test2", s: "a", want: "a"},
+		{name: "test3", s: "aab", want: "baa"},
+		{name: "test4", s: "zzZ", want: "Zzz"},
+		{name: "test5", s: "ab2", want: "2ba"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverse(tt.s); got != tt.want {
+				t.Errorf("reverse() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
